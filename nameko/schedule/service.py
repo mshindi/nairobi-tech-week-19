78d0e2b54b5331cebe4982780defcdec07044c0a
victormshindi <victor.mshindi@twigafoods.com>
2019-04-26 10:22:20 +0300

    update schedule service

--- nameko/schedule/service.py
+++ nameko/schedule/service.py
@@ -1,98 +1,23 @@
 import json
-import newrelic.agent
-from app.entrypoints import http
-from nameko_sqlalchemy import Database
+from nameko.web.handlers import http
 
-from app.auth import ContextData, authorize
-from app.exceptions import BadRequest
-from app.database import Base, create_session
-from app.processing.pipelines import Pipeline
-from app.graphql.query import graphql_query, parse_body
-from app.health_check import HealthCheck
-from nameko.events import EventDispatcher
+class ScheduleHttpService:
+    name = "http_schedule_service"
+
+    @http('GET', '/schedule')
+    def get_schedule(self, request):
+        with open('data.json') as file:
+            data = json.load(file)
+        return json.dumps(data)
+
+    @http('GET', '/location/<int:value>')
+    def get_schedule_id(self, request, value):
+        with open('data.json') as file:
+            data = json.load(file)
+        for item in data:
+            if item["schedule_id"] == str(value):
+                return json.dumps(item)
+            else:
+                return json.dumps({404: "Not found"})
 
 
-newrelic.agent.initialize()
-
-
-class HTTPProcessingService:
-    name = "http_processing_service"
-    db = Database(Base)
-    context = ContextData()
-    dispatch = EventDispatcher()
-
-    def __init__(self):
-        pass
-
-    @http('GET', '/processing/initdb')
-    def initdb(self, request):
-        '''
-        Allow for manual initialization of the database.
-        '''
-        create_session()
-        return json.dumps({"success": True})
-
-    @http('GET', '/processing/health')
-    def health_check(self, request):
-        # pylint: disable=unused-argument
-        session = self.db.get_session()
-        return HealthCheck(session).service_health_check()
-
-    ######################################################################
-    #  STOCK AVAILABILITY
-    ######################################################################
-
-    @http('GET', '/stock_availability')
-    @authorize('can-view-stock_availability')
-    def list_stocks(self, request):
-        session = self.db.get_session()
-        per_page = request.args.get('per_page')
-        page = request.args.get('page')
-        return Pipeline(session).list_stock(per_page, page)
-
-    @http('GET', '/stock_availability/<uuid:stock_availability_id>')
-    @authorize('can-view-stock_availability')
-    def product_details(self, request, stock_availability_id):
-        session = self.db.get_session()
-        return Pipeline(session).get_stock_by_id(stock_availability_id)
-
-    @http('POST', '/stock_availability')
-    @authorize('can-add-stock_availability')
-    def add_stock(self, request):
-        session = self.db.get_session()
-        try:
-            data = json.loads(request.data)
-        except ValueError as e:
-            raise BadRequest("Invalid json: {}".format(e))
-        created_by = request.headers.get("X-Authenticated-Userid", "Unknown")
-        data["created_by"] = created_by
-        data["updated_by"] = created_by
-        return Pipeline(session, dispatch=self.dispatch).add_stock(data)
-
-    @http('PUT', '/stock_availability/<uuid:stock_availability_id>')
-    @authorize('can-update-stock_availability')
-    def edit_stock(self, request, stock_availability_id):
-        session = self.db.get_session()
-        try:
-            data = json.loads(request.data)
-        except ValueError as e:
-            raise BadRequest("Invalid json: {}".format(e))
-        data["updated_by"] = request.headers.get(
-            "X-Authenticated-Userid", "Unknown")
-        data["stock_availability_id"] = stock_availability_id
-        return Pipeline(session, dispatch=self.dispatch).update_stock(data)
-
-    @http('DELETE', '/stock_availability/<uuid:stock_availability_id>')
-    @authorize('can-update-stock_availability')
-    def delete_stock(self, request, stock_availability_id):
-        session = self.db.get_session()
-        done_by = request.headers.get("X-Authenticated-Userid", "Unknown")
-        return Pipeline(session).delete_stock(stock_availability_id, done_by)
-
-    ######################################################################
-    #  GRAPHQL
-    ######################################################################
-
-    @http('POST', '/graphql')
-    def query(self, request):
-        return graphql_query(parse_body(request), self.db.get_session())
